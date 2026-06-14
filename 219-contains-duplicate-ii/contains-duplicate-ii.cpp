72858a0adc994a9f3d99e271a0ea1aaaf41e847d
Aryan Saxena <contact.aryan.dev@gmail.com>
2026-06-14 22:41:22 +0530

    Time: 77 ms (45.16%) | Memory: 93.8 MB (79.21%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         //this is just to make the code work at its maximum speed without the need to worry about sycning with C. original complexity is still O(n)
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
         unordered_set<int> s;
         for(int i = 0; i < nums.size(); i++){
